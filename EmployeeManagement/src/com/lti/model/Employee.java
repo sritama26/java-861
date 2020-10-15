5e3e14234a9e12ae5f7e311b46496579be2d65b3
Sritama Ghosh <sritamaghosh2698@gmail.com>
2020-10-15 13:20:31 +0530

    Added Saved Employee class

--- EmployeeManagement/src/com/lti/model/Employee.java
+++ EmployeeManagement/src/com/lti/model/Employee.java
@@ -2,4 +2,24 @@ package com.lti.model;
 
 public class Employee {
 
+	private int employeeID;
+	private String employeeName;
+	public Employee() {
+		//super();
+	}
+	public int getEmployeeID() {
+		return employeeID;
+	}
+	public void setEmployeeID(int employeeID) {
+		this.employeeID = employeeID;
+	}
+	public String getEmployeeName() {
+		return employeeName;
+	}
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+	
+	
+	
 }
