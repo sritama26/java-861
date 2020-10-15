e2c1b9e6fbe1eac189ce155dad3e6b2d524b7336
Sritama Ghosh <sritamaghosh2698@gmail.com>
2020-10-15 13:26:10 +0530

    Added Department class

---
+++ EmployeeManagement/src/com/lti/model/Department.java
@@ -0,0 +1,21 @@
+package com.lti.model;
+
+public class Department {
+	
+	int deptId;
+	String deptName;
+	public int getDeptId() {
+		return deptId;
+	}
+	public void setDeptId(int deptId) {
+		this.deptId = deptId;
+	}
+	public String getDeptName() {
+		return deptName;
+	}
+	public void setDeptName(String deptName) {
+		this.deptName = deptName;
+	}
+	
+
+}
